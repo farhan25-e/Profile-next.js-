d5bf8b676535cae5c9cf77f6036af6977d99445a
Farhan <ashfaqfarhan495@gmail.com>
2026-09-19 22:03:07 -0700

    Update Project

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -56,7 +56,7 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/">
+          <Link href="https://github.com/farhan25-e">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="linkedin.com">
